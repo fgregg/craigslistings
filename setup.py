722eba884dac41a097e40c0b6ae442423e76d08a
Forest Gregg <fgregg@uchicago.edu>
2016-05-13 18:07:31 -0500

    simplify setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,11 @@ setup(
     install_requires=['psycopg2', 'raven'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Cython', 
         'Topic :: Software Development :: Libraries :: Python Modules']
     )
 
